2b4911088013396badae28ac1ad3e3f6098ba4ff
brandonroycstn <brandonroycstn@hotmail.com>
2016-12-01 20:21:13 -0600

    Heroku cli is installed with packages, comments were added

--- config/globalVars.js
+++ config/globalVars.js
@@ -6,8 +6,9 @@ module.exports = {
   secret: 'salt',
   ids: {
     facebook: {
-      clientID: '1202674773150793',
-      clientSecret: '9dd2a26ca2e1c05b2328a1cb2c12a108',
+      clientID: '0000000000000000',
+      //randomly generated string of letters and numbers
+      clientSecret: 'f0kusz2l319rb6758pe4qwhagontjmxyd',
       callbackURL: 'http://localhost:3000/facebook/callback'
     }
   }
